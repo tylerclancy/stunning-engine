7b081c277ca6b972d4929749ec7dc395dd0d7ad4
Tyler <tylerclancy030@gmail.com>
2022-05-25 19:23:35 -0500

    Build simple HTTP server and display generated data

---
+++ server/index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+import Chance from 'chance';
+const chance = new Chance();
+
+const people = [...Array(250).keys()].map(id => {
+  return {
+    id,
+    type: chance.profession(),
+    age: chance.age(),
+    name: chance.name()
+  }
+});
+
+app.get('', (req, res) => {
+  const q = req.query.q?.toLowerCase() || '';
+  const results = people.filter(profession => profession.type.toLowerCase().includes(q));
+
+  res.send(results);
+});
+
+app.listen(4000, () => {
+  console.log('Server started on port 4000.');
+});
